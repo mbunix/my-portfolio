ccce16ede3721acc0ef8d852774814bb3c9fd23e
mbunix <bellianotieno24@gmal.com>
2023-08-01 16:28:57 +0300

    changes on development : pages

---
+++ src/components/pages/landingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from '../sidebar/sidebar'
+function LandingPage() {
+  return (
+      <div>
+          <Sidebar/>
+      <h2 className="text-3xl font-bold underline">Landing Page</h2>
+    </div>
+  )
+}
+
+export default LandingPage
---
+++ src/components/sidebar/sidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {
+    CDBsidebar,
+} from 'cdbreact'
+function Sidebar() {
+  return (
+    <div>
+          <CDBsidebar>
+              
+      </CDBsidebar>
+    </div>
+  )
+}
+
+export default Sidebar
