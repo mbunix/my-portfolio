7fe4efb680641b92338f8c900ea065c2ad3e3082
mbunix <bellianotieno24@gmal.com>
2023-08-01 16:43:45 +0300

    began writing tests on -b tests

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
 import {
-    CDBsidebar,
+ CDBSidebar,
+  CDBSidebarContent,
+  CDBSidebarFooter,
+  CDBSidebarHeader,
+  CDBSidebarMenu,
+  CDBSidebarMenuItem,
 } from 'cdbreact'
 function Sidebar() {
   return (
-    <div>
-          <CDBsidebar>
-              
-      </CDBsidebar>
+      <div>
+          <CDBSidebar>
+              <CDBSidebarHeader>
+                  <CDBSidebarContent>
+                      <CDBSidebarMenu>
+                          <CDBSidebarMenuItem>
+                              <NavLink to='/'>Home</NavLink>
+                          </CDBSidebarMenuItem>
+                          <CDBSidebarMenuItem>
+                              <NavLink to='/services'>Services</NavLink>
+                          </CDBSidebarMenuItem>
+                          <CDBSidebarMenuItem>
+                              <NavLink to='/about'>About</NavLink>
+                          </CDBSidebarMenuItem>
+                      </CDBSidebarMenu>
+                  </CDBSidebarContent>
+              </CDBSidebarHeader>
+          </CDBSidebar>
     </div>
   )
 }
